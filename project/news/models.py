e628ebed714938c72610133128645bfbcf904222
Iliya <6262140@gmail.com>
2022-09-14 22:06:52 +0300

    Add detail & index

--- project/news/models.py
+++ project/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
+class New(models.Model):
+    title = models.CharField(max_length=64)
+    Text = models.TextField()
+    date_pub = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=128, unique=True)
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'slug': self.slug})
+    
+    
+    def __str__(self):
+        return '{}'.format(self.title)
+    
+    
